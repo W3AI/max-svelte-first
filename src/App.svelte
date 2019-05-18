3dc5a3c59bed9800a7296652a514deab05b65be4
stefian <v_ianta@yahoo.com>
2019-05-18 10:48:04 -0400

    added uppercaseName as a $: reactive var

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,13 @@
     let name = 'Steve';
     let age = 36;
 
+    // let uppercaseName; not required
+
+    // js labeled statement "<label-word>:"
+    // in svelte it must be "$:"
+    // svelte will recalculate var after $: whenever params change
+    $: uppercaseName = name.toUpperCase();
+
     function incrementAge() {
         age += 1;
     }
@@ -13,5 +20,5 @@
 	}
 </style>
 
-<h1>Servus {name}, my age is {age}!</h1>
+<h1>Servus {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
