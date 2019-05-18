b334b7c4052ba5d01bfab0265db32c6ae28ffd12
stefian <v_ianta@yahoo.com>
2019-05-18 12:08:49 -0400

    added 2 way binding with bind:value={name}

--- src/App.svelte
+++ src/App.svelte
@@ -38,4 +38,5 @@
 <h1>Servus {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
 <!-- <button on:click="{changeName}">Change Name</button> -->
-<input type="text" value="{name}" on:input="{nameInput}" >
+<!-- <input type="text" value="{name}" on:input="{nameInput}" > -->
+<input type="text" bind:value="{name}" >
