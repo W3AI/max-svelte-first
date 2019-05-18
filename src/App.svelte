44b17229a2f0db3df2290ba12e872b6e2d95ac0d
stefian <v_ianta@yahoo.com>
2019-05-18 08:58:02 -0400

    added button with on:click to ingrmentAge

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
     let name = 'Steve';
-    let age = 50;
+    let age = 36;
 
     function incrementAge() {
         age += 1;
@@ -14,4 +14,4 @@
 </style>
 
 <h1>Servus {name}, my age is {age}!</h1>
-<button>Change Age</button>
+<button on:click="{incrementAge}">Change Age</button>
